34547c1e266206bc3b82e93fda2af222e185c544
TONY <osmanetoni@gmail.com>
2025-02-24 22:19:14 -0300

    added scanner so that the user can insert his own data

--- src/principal.java
+++ src/principal.java
@@ -1,12 +1,29 @@
+import javax.sound.midi.SysexMessage;
+import java.util.Scanner;
+
 public class principal {
     public static void main(String[] args) {
         Cliente c1 = new Cliente();
-        c1.setIdCliente(3);
-        c1.setNome("Samuel Azevedo");
-        c1.setEmail("tonyosman.fiap@gmail.com");
-        c1.setCpf("888.777.555-00");
-        c1.setTelefone("(11)75878-000");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("DIGITE O CODIGO DO CLIENTE -> ");
+        c1.setIdCliente(sc.nextInt());
+
+        System.out.println("DIGITE O NOME DO CLIENTE -> ");
+        c1.setNome(sc.next());
+
+        System.out.println("DIGITE O EMAIL DO CLIENTE -> ");
+        c1.setEmail(sc.next());
+
+        System.out.println("DIGITE O CPF -> ");
+        c1.setCpf(sc.next());
+
+        System.out.println("NUMERO DO CLIENTE -> ");
+        c1.setTelefone(sc.next());
+
         c1.insereCliente();
+
+
 
 
     }
